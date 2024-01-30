0b79c371db364259d7bd0b703243be959d1418de
Pranav Kurundodi <111072474+PranavKurundodi@users.noreply.github.com>
2024-01-31 01:05:35 +0530

    Add files via upload

---
+++ File Uploader Bot Slack API/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/slack-go/slack"
+)
+
+func main() {
+	os.Setenv("SLACK_BOT_TOKEN", "") // Type your slack bot token in the quotation marks
+	os.Setenv("CHANNEL_ID", "")      // Type your channel id where you want to upload the file in the quotation marks
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	channelArr := []string{os.Getenv("CHANNEL_ID")}
+	fileArr := []string{""} // Type your file name which you want to upload in the channel in the quotation marks
+
+	for i := 0; i < len(fileArr); i++ {
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File:     fileArr[i],
+		}
+		file, err := api.UploadFile(params)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+		fmt.Printf("Name: %s, URL:%s\n", file.Name, file.URL)
+	}
+}
